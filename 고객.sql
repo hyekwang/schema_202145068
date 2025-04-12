287d99b8c29ec49dd52f046f5a7c09f8f8ccbd70
hyekwang <hyekwang1227@naver.com>
2025-04-12 19:55:14 +0900

    Create 고객.sql

---
+++ 고객.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+    customer_id CHAR(5) PRIMARY KEY,
+    company_name VARCHAR(30),
+    contact_name VARCHAR(20),
+    contact_title VARCHAR(50),
+    address VARCHAR(50),
+    city VARCHAR(20),
+    region VARCHAR(20),
+    phone_number VARCHAR(20),
+    mileage INT
+);
