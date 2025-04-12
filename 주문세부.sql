c806e49eb7f42c080295727ac54ed46345e2b115
hyekwang <hyekwang1227@naver.com>
2025-04-12 19:57:41 +0900

    Update 주문세부.sql

--- 주문세부.sql
+++ 주문세부.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE order_details (
+    order_id CHAR(5),
+    product_id INT,
+    unit_price INT,
+    quantity INT,
+    discount FLOAT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
