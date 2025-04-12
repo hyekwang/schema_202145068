e5bb14de3aed1a4e6a5ed325a8eec05c56134f5f
hyekwang <hyekwang1227@naver.com>
2025-04-12 19:58:44 +0900

    Update 사원.sql

--- 사원.sql
+++ 사원.sql
@@ -1,1 +1,19 @@
-
+CREATE TABLE employees (
+    employee_id CHAR(3) PRIMARY KEY,
+    name VARCHAR(20),
+    english_name VARCHAR(20),
+    title VARCHAR(10),
+    gender CHAR(2),
+    birth_date DATE,
+    hire_date DATE,
+    address VARCHAR(50),
+    city VARCHAR(20),
+    region VARCHAR(20),
+    contact_title VARCHAR(20),
+    manager_id CHAR(3),
+    department_id CHAR(2),
+    assistant_id CHAR(3),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (assistant_id) REFERENCES employees(employee_id)
+);
